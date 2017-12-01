a8b17b36cff4c019e006414d85c27260a1cf0120
“Ana” <amartinezaguilar6@gmail.com>
2017-12-01 12:44:13 +0100

    [Create]prompts to fill the cv (name, city, email)

--- main.js
+++ main.js
@@ -13,3 +13,30 @@ function addYears(initYear, currentYear) {
 var selectYears = document.querySelector('#selectorYear');
 
 selectorYear.innerHTML = addYears(1950, 2017);
+
+
+//Función para prompt en cv primera versión
+
+    //Nombre y apellidos
+
+var name = prompt('Dime tu nombre y dos apellidos');
+
+var nameCV = document.querySelector('.cv-name');
+
+nameCV.innerHTML = name;
+
+    //Ciudad
+
+var city = prompt('Dime tu ciudad');
+
+var cityCV = document.querySelector('#city-cv');
+
+cityCV.innerHTML = city;
+
+    //Correo electrónico
+
+var email = prompt('Dime tu email');
+
+var emailCV = document.querySelector('#email-cv');
+
+emailCV.innerHTML = email;
