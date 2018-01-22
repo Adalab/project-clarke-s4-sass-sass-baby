3ca527633c7c34ccfd5f1cc4a4dfec845df41611
Adalaber <mvtercero85@gmail.com>
2018-01-22 20:33:34 +0100

    Clean code

--- src/App.js
+++ src/App.js
@@ -5,11 +5,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Holi
         </p>
       </div>
     );
