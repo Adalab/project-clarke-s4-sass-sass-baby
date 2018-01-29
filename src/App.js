42a9ab8381df3aa789cead15282c7325b781ee98
Ana Gonzalez <anagonm@gmail.com>
2018-01-29 19:19:30 +0100

    Finished print function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+
 import React, { Component } from 'react';
 import './index.css';
 import Header from './components/Header';
@@ -8,26 +10,75 @@ import ButtonsForm from './components/ButtonsForm';
 import RightCv from './components/RightCv';
 import LeftCv from './components/LeftCv';
 
+// import './print_media_query.scss'
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <main>
-      <Header/>
-      <div className="formandcv">
-      <SectionGeneral/>
-      <div className="cv-content">
-        <aside>
-          <div className="print-cv">
+
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      printing: false
+    }
+
+  }
+
+  componentDidMount() {
+    var self = this;
+    window.onafterprint = () => {
+      self.setState({printing: false})
+    };
+  }
+
+
+  imprimir() {
+    this.setState({printing: true})
+    setTimeout(() => {
+      window.print();
+    }, 1000);
+
+  }
+
+
+  renderApplication() {
+
+      if (this.state.printing === true) {
+        return (
+          <div className="print-cv" style={{height: '100%', margin: 0}}>
             <LeftCv/>
             <RightCv/>
           </div>
-          <input type="button" className="buttonPrint" defaultValue="Imprimir" />
-        </aside>
-      </div>
-      </div>
-      </main>
-      </div>
+        )
+      }
+
+      return (
+        <div className="App">
+          <iframe id="ifmcontentstoprint" style={{height: '0px', width: '0px', position: 'absolute'}}></iframe>
+          <main>
+            <Header/>
+            <div className="formandcv">
+              <SectionGeneral/>
+              <div className="cv-content">
+                <aside>
+                  <div className="print-cv">
+                    <LeftCv/>
+                    <RightCv/>
+                  </div>
+                  <input type="button" onClick={ this.imprimir.bind(this) } className="buttonPrint" defaultValue="Imprimir" />
+                </aside>
+              </div>
+            </div>
+          </main>
+        </div>
+      )
+  }
+
+
+  render() {
+
+    return (
+      this.renderApplication()
     );
   }
 }
