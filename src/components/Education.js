7f2677da7b638e5651bfb9e17fca2cfad695b1f6
Belene <be23mg@gmail.com>
2018-02-04 20:24:41 +0100

    [Changes]inputs experience and education

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,26 +6,26 @@ class Education extends Component {
 
   render() {
     const handleChange = (event) => {
-      const name = event.target.id;
+      const name = event.target.name;
       this.props.handleChange(name, event.target.value);
     };
     return (
       <Fieldset title="Formación">
         <Colapsable box= "firstEducationBox" link="Añadir formación 1">
           <div className="drop-down">
-          <input onChange={handleChange} className="input-form title-education main-value" type="text" name="title-education" placeholder="Título" required />
-          <input onChange={handleChange} className="input-form center-education" type="text" name="company-project" placeholder="Centro de formación" required />
-          <input onChange={handleChange} className="input-form date-start-education" type="date" name="start-date-project" placeholder="Fecha inicio" required />
-          <input onChange={handleChange} className="input-form date-end-education" type="date" name="finish-date-project" placeholder="Fecha fin" required />
+          <input onChange={handleChange} className="input-form title-education main-value" type="text" name="titleEducation" placeholder="Título" required />
+          <input onChange={handleChange} className="input-form center-education" type="text" name="trainingCenterEducation" placeholder="Centro de formación" required />
+          <input onChange={handleChange} className="input-form date-start-education" type="date" name="startDateEducation" placeholder="Fecha inicio" required />
+          <input onChange={handleChange} className="input-form date-end-education" type="date" name="finishDateEducation" placeholder="Fecha fin" required />
           </div>
         </Colapsable>
 
         <Colapsable box= "secondEducationBox" link="Añadir formación 2">
         <div className="drop-down">
-          <input onChange={handleChange} className="input-form title-education1 main-value" type="text" name="title-education1" placeholder="Título" required />
-          <input onChange={handleChange} className="input-form center-education1" type="text" name="company-project" placeholder="Centro de formación" required />
-          <input onChange={handleChange} className="input-form date-start-education1" type="date" name="company-project" placeholder="Fecha inicio" required />
-          <input onChange={handleChange} className="input-form date-end-education1" type="date" name="company-project" placeholder="Fecha inicio" required />
+          <input onChange={handleChange} className="input-form title-education1 main-value" type="text" name="titleEducation2" placeholder="Título" required />
+          <input onChange={handleChange} className="input-form center-education1" type="text" name="trainingCenterEducation2" placeholder="Centro de formación" required />
+          <input onChange={handleChange} className="input-form date-start-education1" type="date" name="startDateEducation2" placeholder="Fecha inicio" required />
+          <input onChange={handleChange} className="input-form date-end-education1" type="date" name="finishDateEducation2" placeholder="Fecha inicio" required />
         </div>
         </Colapsable>
       </Fieldset>
--- src/components/ExperienceBox.js
+++ src/components/ExperienceBox.js
@@ -7,23 +7,23 @@ class ExperienceBox extends Component {
 			<h3 className="seccion tittle-experience">EXPERIENCIA</h3>
 			{/*   EXPERIENCE 1 -------------*/}
 			<ul className="list-none">
-				<li className="cv-name-company bold">Nombre empresa</li>
-				<li className="cv-name-position">Cargo ejercido</li>
-				<li className="cv-date-start-experience">Fecha inicio</li>
-				<li className="cv-date-end-experience">Fecha fin</li>
+				<li className="cv-name-company bold">{ this.props.cv.companyName }</li>
+				<li className="cv-name-position">{ this.props.cv.titleExperiencie }</li>
+				<li className="cv-date-start-experience">{ this.props.cv.startDateExperience }</li>
+				<li className="cv-date-end-experience">{ this.props.cv.finishDateExperience }</li>
 			</ul>
 			<ul className="list-style cv-description-experience">
-				<li className="cv-description-experience">Tarea realizada</li>
+				<li className="cv-description-experience">{ this.props.cv.descriptionExperience }</li>
 			</ul>
 			{/*   EXPERIENCE 2 -------------*/}
 			<ul className="list-none">
-				<li className="cv-name-company1 bold margin-top">Nombre empresa</li>
-				<li className="cv-name-position1">Cargo ejercido</li>
-				<li className="cv-date-start-experience1">Fecha inicio</li>
-				<li className="cv-date-end-experience1">Fecha fin</li>
+				<li className="cv-name-company1 bold margin-top">{ this.props.cv.companyName2 }</li>
+				<li className="cv-name-position1">{ this.props.cv.titleExperiencie2 }</li>
+				<li className="cv-date-start-experience1">{ this.props.cv.startDateExperience2 }</li>
+				<li className="cv-date-end-experience1">{ this.props.cv.finishDateExperience2 }</li>
 			</ul>
 			<ul className="list-style ul-experience">
-				<li className="cv-description-experience1">Tarea realizada</li>
+				<li className="cv-description-experience1">{ this.props.cv.descriptionExperience2 }</li>
 			</ul>
 		</div>
   );
