08d4c106a3b4295ed42095862eb4230c5d4a13eb
Belene <be23mg@gmail.com>
2018-02-06 00:18:45 +0100

    [Add]button delete styles

--- src/components/Colapsable.js
+++ src/components/Colapsable.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 class Colapsable extends Component {
   constructor(props){
-    super(props)
-    this.state={
+    super(props);
+    this.state = {
       collapsed:true
-    }
-  this.handleClick=this.handleClick.bind(this);
+    };
+    this.handleClick=this.handleClick.bind(this);
   }
 
   handleClick(){
@@ -15,24 +15,22 @@ class Colapsable extends Component {
 
   render(){
     const collapse = this.state.collapsed ? 'hidden' : '';
-
     return(
       <div  id={this.props.box}>
-      <div className="add-project caja-educacion">
-      <div className="addfield" data-id={0}>
-      <a className="textadd">{this.props.link}</a>
-      </div>
+        <div className="add-project caja-educacion">
+          <div className="addfield" data-id={0}>
+            <a className="textadd">{this.props.link}</a>
+          </div>
 
-      <div className="buttons-project-container">
-      <img id="arrowDownFirstElement" onClick={this.handleClick} className="buttons-project flecha-abajo" src="img/arrowdown.png" alt="Flecha hacia abajo" />
-      </div>
-      </div>
+          <div className="buttons-project-container">
+            <img id="arrowDownFirstElement" onClick={this.handleClick} className="buttons-project flecha-abajo" src="img/arrowdown.png" alt="Flecha hacia abajo" />
+          </div>
+        </div>
 
-      <div className= {`drop-down ${collapse}`}>
-      {this.props.children}
+        <div className= {`drop-down ${collapse}`}>
+          {this.props.children}
+        </div>
       </div>
-      </div>
-
     );
   }
 
