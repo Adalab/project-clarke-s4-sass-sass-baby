f39ecd3e9a74a3a00a925d6c818523451884e290
Adalaber <mvtercero85@gmail.com>
2018-02-05 19:04:21 +0100

    Test button delete

--- src/components/Colapsable.js
+++ src/components/Colapsable.js
@@ -4,16 +4,29 @@ class Colapsable extends Component {
   constructor(props){
     super(props)
     this.state={
-      collapsed:true
+      collapsed:true,
+      name: this.name
     }
-  this.handleClick=this.handleClick.bind(this);
+    this.handleClick=this.handleClick.bind(this);
+    this.deleteForm=this.deleteForm.bind(this);
   }
 
   handleClick(){
     this.setState({collapsed : !this.state.collapsed});
   }
 
+  deleteForm(){
+    if (! this.name === 0){
+      return this.name= '';
+    }
+  }
+
   render(){
+
+    const handleChange = (event) => {
+      const name = event.target.name;
+      this.props.handleChange(name, event.target.value);
+    };
 
     const collapse = this.state.collapsed ? 'hidden' : '';
 
@@ -25,7 +38,7 @@ class Colapsable extends Component {
       </div>
 
       <div className="buttons-project-container">
-      <img className="buttons-project margin-right-buttons papelera" src="img/trash.png" alt="Icono de papelera" data-id={0} />
+      <img onClick= {this.deleteForm} className="buttons-project margin-right-buttons papelera" src="img/trash.png" alt="Icono de papelera" />
       <img id="arrowUpFirstElement" className="buttons-project margin-right-buttons" src="img/arrowup.png" alt="Flecha hacia arriba" />
       <img id="arrowDownFirstElement" className="buttons-project flecha-abajo" src="img/arrowdown.png" alt="Flecha hacia abajo" />
       </div>
