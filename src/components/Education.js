b69a24dee07fa0ae6d46b873ed16daf747a75466
Adalaber <mvtercero85@gmail.com>
2018-02-05 23:41:03 +0100

    Addbutton delete in Experience component

--- src/components/Education.js
+++ src/components/Education.js
@@ -14,7 +14,7 @@ class Education extends Component {
       <Fieldset title="Formación">
         <Colapsable box= "firstEducationBox" link="Añadir formación 1" ref="firstEducationBox">
           <div className="drop-down">
-            <Input onChange={handleChange} className="input-form title-education main-value" type="text" name="titleEducation" placeholder="Título" value={this.props.data.titleEducation} deleteState={this.props.deleteState} required />
+            <Input onChange={handleChange} className="input-form title-education main-value" type="text" name="titleEducation" placeholder="Título" value={this.props.data.titleEducation} deleteState={this.props.deleteState} required={true} />
             <Input onChange={handleChange} className="input-form center-education" type="text" name="trainingCenterEducation" placeholder="Centro de formación" value={this.props.data.trainingCenterEducation} deleteState={this.props.deleteState} required={true}/>
             <Input onChange={handleChange} className="input-form date-start-education" type="date" name="startDateEducation" placeholder="Fecha inicio" value={this.props.data.startDateEducation} deleteState={this.props.deleteState} required={true} />
             <Input onChange={handleChange} className="input-form date-end-education" type="date" name="finishDateEducation" placeholder="Fecha fin" value={this.props.data.finishDateEducation} deleteState={this.props.deleteState} required={true} />
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Fieldset from './Fieldset';
 import Colapsable from './Colapsable';
+import Input from './Input';
 
 class Experience extends Component {
 
@@ -11,24 +12,25 @@ class Experience extends Component {
     };
     return (
       <Fieldset title= "Experiencia">
-        <Colapsable box="firstExperienceBox" link="Añadir experiencia 1">
-        <div className="drop-down">
-          <input onChange={handleChange} className="input-form title-education main-value" type="text" name="companyName" placeholder="Empresa" required />
-          <input onChange={handleChange} className="input-form center-education" type="text" name="titleExperiencie" placeholder="Puesto de trabajo" required />
-          <input onChange={handleChange} className="input-form date-start-education" type="date" name="startDateExperience" placeholder="Fecha inicio" required />
-          <input onChange={handleChange} className="input-form date-end-education" type="date" name="finishDateExperience" placeholder="Fecha fin" required />
-        </div>
-        </Colapsable>
+      <Colapsable box="firstExperienceBox" link="Añadir experiencia 1">
+      <div className="drop-down">
+      <Input onChange={handleChange} className="input-form title-education main-value" type="text" name="companyName" placeholder="Empresa" value={this.props.data.companyName} deleteState={this.props.deleteState} required={true}/>
+      <Input onChange={handleChange} className="input-form center-education" type="text" name="titleExperiencie" placeholder="Puesto de trabajo" value={this.props.data.titleExperiencie} deleteState={this.props.deleteState} required={true}/>
+      <Input onChange={handleChange} className="input-form date-start-education" type="date" name="startDateExperience" placeholder="Fecha inicio" value={this.props.data.startDateExperience} deleteState={this.props.deleteState} required={true}/>
+      <Input onChange={handleChange} className="input-form date-end-education" type="date" name="finishDateExperience" placeholder="Fecha fin" value={this.props.data.finishDateExperience} deleteState={this.props.deleteState} required={true}/>
+      <textarea onChange={handleChange} className="input-form description-experience" name="descriptionExperience" rows={8} cols={80} defaultValue={"                      "} value={this.props.data.descriptionExperience} deleteState={this.props.deleteState} required={true}/>
+      </div>
+      </Colapsable>
 
-        <Colapsable box="secondExperienceBox" link="Añadir experiencia 2">
-        <div className="drop-down">
-          <input onChange={handleChange} className="input-form main-value name-company" id="company" type="text" name="companyName2" placeholder="Empresa" required />
-          <input onChange={handleChange} className="input-form name-position" id="position" type="text" name="titleExperiencie2" placeholder="Puesto de trabajo" required />
-          <input onChange={handleChange} className="input-form date-start-experience" id="init-data-experience" type="date" name="startDateExperience2" required />
-          <input onChange={handleChange} className="input-form date-end-experience" id="finish-data-experience" type="date" name="finishDateExperience2" required />
-          <textarea onChange={handleChange} className="input-form description-experience" name="descriptionExperience" rows={8} cols={80} defaultValue={"                      "} />
-        </div>
-        </Colapsable>
+      <Colapsable box="secondExperienceBox" link="Añadir experiencia 2">
+      <div className="drop-down">
+      <Input onChange={handleChange} className="input-form main-value name-company" id="company" type="text" name="companyName2" placeholder="Empresa" value={this.props.data.companyName2} deleteState={this.props.deleteState} required={true}/>
+      <Input onChange={handleChange} className="input-form name-position" id="position" type="text" name="titleExperiencie2" placeholder="Puesto de trabajo" value={this.props.data.titleExperiencie2} deleteState={this.props.deleteState} required={true}/>
+      <Input onChange={handleChange} className="input-form date-start-experience" id="init-data-experience" type="date" name="startDateExperience2" value={this.props.data.startDateExperience2} deleteState={this.props.deleteState} required={true}/>
+      <Input onChange={handleChange} className="input-form date-end-experience" id="finish-data-experience" type="date" name="finishDateExperience2" value={this.props.data.finishDateExperience2} deleteState={this.props.deleteState} required={true}/>
+      <textarea onChange={handleChange} className="input-form description-experience" name="descriptionExperience2" rows={8} cols={80} defaultValue={"                      "} value={this.props.data.descriptionExperience2} deleteState={this.props.deleteState} required={true}/>
+      </div>
+      </Colapsable>
       </Fieldset>
     );
   }
